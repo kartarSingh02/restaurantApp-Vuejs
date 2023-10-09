8a0fa29c7c19c589f1e81a01d8dd04f9af4fcdc4
Kartar Singh <k4kartar4520@gmail.com>
2023-10-09 23:21:41 +0530

    fixes

--- src/components/NavbarView.vue
+++ src/components/NavbarView.vue
@@ -5,8 +5,13 @@
         <router-link to="/add">Add restaurant</router-link>
         <router-link to="/update">Update restaurant</router-link>
     </div>
-    <div class="logout">
-        <a href="#" v-on:click="logout">Logout</a>
+    <div class="side">
+        <div class="profile">
+            <p class="para">Hi,{{ name }}</p>
+        </div>
+        <div class="logout">
+            <a href="#" v-on:click="logout">Logout</a>
+        </div>
     </div>
 </div>
 </template>
@@ -14,11 +19,20 @@
 <script>
 export default {
     name:'NavbarView',
+    data(){
+        return{
+            name:''
+        }
+    },
     methods:{
         logout(){
             localStorage.clear();
             this.$router.push({ name: "LogIn" });
         }
+    },
+    mounted(){
+        let user=localStorage.getItem('user-info');
+        this.name=JSON.parse(user).name
     }
 }
 
@@ -44,4 +58,17 @@ export default {
     border-radius: 2px;
     padding:3px;
 }
+.side{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.profile p{
+    color: white;
+    padding: 4px;
+    margin: 8px;
+    font-size: 12px;
+}
+
 </style>
